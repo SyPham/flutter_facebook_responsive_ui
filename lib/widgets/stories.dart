c3c8be35f3100012975c4782939cc6a3d6f6d6da
Henry Pham <phamtiensy8696@gmail.com>
2022-11-07 11:29:15 +0700

    fix _StoryCard

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
@@ -61,64 +63,73 @@ class _StoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(12.0),
-          child: CachedNetworkImage(
-            imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+    return Container(
+      width: 100,
+      child: Stack(
+        children: [
+          // CachedNetworkImage(
+          //     imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+          //     height: double.infinity,
+          //     width: 110.0,
+          //     fit: BoxFit.cover,
+          //   )
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12.0),
+            child: Image.network(
+              isAddStory ? currentUser.imageUrl : story.imageUrl,
+              fit: BoxFit.cover,
+              width: 110,
+              height: double.infinity,
+            ),
+          ),
+          Container(
             height: double.infinity,
             width: 110.0,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Container(
-          height: double.infinity,
-          width: 110.0,
-          decoration: BoxDecoration(
-            gradient: Palette.storyGradient,
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-        ),
-        Positioned(
-          top: 8.0,
-          left: 8.0,
-          child: isAddStory
-              ? Container(
-                  height: 40.0,
-                  width: 40.0,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: const Icon(Icons.add),
-                    iconSize: 30.0,
-                    color: Palette.facebookBlue,
-                    onPressed: () => print('Add to Story'),
-                  ),
-                )
-              : ProfileAvatar(
-                  imageUrl: story.user.imageUrl,
-                  hasBorder: !story.isViewed,
-                ),
-        ),
-        Positioned(
-          bottom: 8.0,
-          left: 8.0,
-          right: 8.0,
-          child: Text(
-            isAddStory ? 'Add to Story' : story.user.name,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+            decoration: BoxDecoration(
+              gradient: Palette.storyGradient,
+              borderRadius: BorderRadius.circular(12.0),
             ),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
           ),
-        ),
-      ],
+          Positioned(
+            top: 8.0,
+            left: 8.0,
+            child: isAddStory
+                ? Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(Icons.add),
+                      iconSize: 30.0,
+                      color: Palette.facebookBlue,
+                      onPressed: () => print('Add to Story'),
+                    ),
+                  )
+                : ProfileAvatar(
+                    imageUrl: story.user.imageUrl,
+                    hasBorder: !story.isViewed,
+                  ),
+          ),
+          Positioned(
+            bottom: 8.0,
+            left: 8.0,
+            right: 8.0,
+            child: Text(
+              isAddStory ? 'Add to Story' : story.user.name,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
